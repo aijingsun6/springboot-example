54ca29660a31aaaf4b4d13acd47557c0732631c4
alking <aijingsun6@gmail.com>
2026-07-18 17:33:08 +0800

    start

--- src/test/java/cc/alking/example/springboot/SpringbootApplicationTests.java
+++ src/test/java/cc/alking/example/springboot/SpringbootApplicationTests.java
@@ -1,13 +1,23 @@
 package cc.alking.example.springboot;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
-@SpringBootTest
+@SpringBootTest(classes = SpringbootApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class SpringbootApplicationTests {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SpringbootApplicationTests.class);
+
+    @Autowired
+    private ApplicationContext ctx;
 
 	@Test
 	void contextLoads() {
-	}
+	    LOGGER.info("ApplicationContext {}", ctx.getClass());
+    }
 
 }
